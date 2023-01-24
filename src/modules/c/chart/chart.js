0f374f6707d74e8ea1fc502ee1594c1680eb719c
Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2023-01-24 12:51:57 +0600

    working d3 chart with settimeout

--- src/modules/c/chart/chart.js
+++ src/modules/c/chart/chart.js
@@ -1,177 +1,72 @@
-import { LightningElement } from 'lwc';
-import { processMorningStarData, getDataForSelectedPlan } from './dataUtil';
+import { LightningElement } from "lwc";
+import { processMorningStarData, getDataForSelectedPlan } from "./dataUtil";
 
 export default class Chart extends LightningElement {
-
   tableData;
   tableDataSize;
   assetValueObj;
   totalAssetValue;
   timeNow;
 
-    morningstarData;/* = {
-        id: "123",
-        tpaId: "tpa_123",
-        tpaFirstname: "William",
-        tpaLastname: "Scott",
-        lastDataUpdatedDate: "07/04/22",
-        assetClassSummary: {
-          totalAssetValue: "8000",
-          assetClasses: [
-            {
-              class: "Stocks",
-              valuePercentage: "10",
-              assetValue: "1000",
-              assetValueChangePercentage: "0.4",
-              changeReference: "Month",
-            },
-            {
-              class: "Bonds",
-              valuePercentage: "20",
-              assetValue: "2000",
-              assetValueChangePercentage: "0.4",
-              changeReference: "Month",
-            },
-            {
-              class: "Balanced",
-              valuePercentage: "35",
-              assetValue: "3500",
-              assetValueChangePercentage: "0.4",
-              changeReference: "Month",
-            },
-            {
-              class: "Cash",
-              valuePercentage: "12",
-              assetValue: "1200",
-              assetValueChangePercentage: "0.4",
-              changeReference: "Month",
-            },
-            {
-              class: "Other",
-              valuePercentage: "3",
-              assetValue: "300",
-              assetValueChangePercentage: "0.4",
-              changeReference: "Month",
-            },
-          ],
-        },
-        planDetails: [
-          {
-            planId: "DEMO12",
-            planName: "Acme Marketing",
-            totalAssetValue: "600",
-            assetClasses: [
-              {
-                class: "Stocks",
-                valuePercentage: "10",
-                assetValue: "100",
-                assetValueChangePercentage: "0.4",
-                changeReference: "Month",
-              },
-              {
-                class: "Bonds",
-                valuePercentage: "20",
-                assetValue: "200",
-                assetValueChangePercentage: "-0.4",
-                changeReference: "Month",
-              },
-              {
-                class: "Balanced",
-                valuePercentage: "30",
-                assetValue: "300",
-                assetValueChangePercentage: "0.4",
-                changeReference: "Month",
-              },
-            ],
-          }
-          ,
-          {
-            planId: "DEMO22",
-            planName: "Butterfly Marketing",
-          }
-        ]
-      };
-      */
+  morningstarData;
 
-      getMorningStarData() {
-        setTimeout(() => {
-          fetch('https://twentytwo.github.io/mock-api/morningstar.json')
-          .then(response => response.json())
-          .then(response_data => {
-            this.morningstarData = response_data;
-            let structuredData  = processMorningStarData(this.morningstarData);  
-            console.log('----StructuredData------');
-            console.log(structuredData);
+  getMorningStarData() {
+    setTimeout(() => {
+      fetch("https://twentytwo.github.io/mock-api/morningstar.json")
+        .then((response) => response.json())
+        .then((response_data) => {
+          this.morningstarData = response_data;
+          let structuredData = processMorningStarData(response_data);
 
-            let {data, assetClassAndValue} = getDataForSelectedPlan(structuredData, 'all_plans');
-            console.log('----data------');
-            console.log(data);
+          let { data, assetClassAndValue } = getDataForSelectedPlan(
+            structuredData,
+            "all_plans"
+          );
 
-            console.log('----tableData------');
-            console.log(data.assetClasses);
-
-            console.log('----assetClassAndValue------');
-            console.log(assetClassAndValue);
-          
           this.totalAssetValue = data.totalAssetValue;
           this.timeNow = new Date().getTime();
           this.tableData = data.assetClasses;
           this.tableDataSize = this.tableData ? this.tableData.length : 0;
 
           this.assetValueObj = assetClassAndValue;
+          // this.template.querySelector('c-d3chart').initializeD3(this.assetValueObj);
         });
-  
-        }, 5000);
-      }
+    }, 500);
+  }
 
-      connectedCallback() {
+  connectedCallback() {
+    this.getMorningStarData();
+  }
 
-        this.getMorningStarData();
-      }
+  loadAllData() {
+    let structuredData = processMorningStarData(this.morningstarData);
 
-      loadAllData() {
-        let structuredData  = processMorningStarData(this.morningstarData);  
-        console.log('----StructuredData------');
-        console.log(structuredData);
+    let { data, assetClassAndValue } = getDataForSelectedPlan(
+      structuredData,
+      "all_plans"
+    );
 
-        let {data, assetClassAndValue} = getDataForSelectedPlan(structuredData, 'all_plans');
-        console.log('----data------');
-        console.log(data);
+    this.totalAssetValue = data.totalAssetValue;
+    this.timeNow = new Date().getTime();
+    this.tableData = data.assetClasses;
+    this.tableDataSize = this.tableData ? this.tableData.length : 0;
 
-        console.log('----tableData------');
-        console.log(data.assetClasses);
+    this.assetValueObj = assetClassAndValue;
+  }
 
-        console.log('----assetClassAndValue------');
-        console.log(assetClassAndValue);
-          
-          this.totalAssetValue = data.totalAssetValue;
-          this.timeNow = new Date().getTime();
-          this.tableData = data.assetClasses;
-          this.tableDataSize = this.tableData ? this.tableData.length : 0;
+  loadPlanData() {
+    let structuredData = processMorningStarData(this.morningstarData);
 
-          this.assetValueObj = assetClassAndValue;
-      }
+    let { data, assetClassAndValue } = getDataForSelectedPlan(
+      structuredData,
+      "DEMO12"
+    );
 
-      loadPlanData() {
-        let structuredData  = processMorningStarData(this.morningstarData);  
-        console.log('----StructuredData------');
-        console.log(structuredData);
+    this.totalAssetValue = data.totalAssetValue;
+    this.timeNow = new Date().getTime();
+    this.tableData = data.assetClasses;
+    this.tableDataSize = this.tableData ? this.tableData.length : 0;
 
-        let {data, assetClassAndValue} = getDataForSelectedPlan(structuredData, 'DEMO12');
-        console.log('----data------');
-        console.log(data);
-
-        console.log('----tableData------');
-        console.log(data.assetClasses);
-
-        console.log('----assetClassAndValue------');
-        console.log(assetClassAndValue);
-          
-          this.totalAssetValue = data.totalAssetValue;
-          this.timeNow = new Date().getTime();
-          this.tableData = data.assetClasses;
-          this.tableDataSize = this.tableData ? this.tableData.length : 0;
-
-          this.assetValueObj = assetClassAndValue;
-      }
+    this.assetValueObj = assetClassAndValue;
+  }
 }
--- src/modules/c/d3chart/d3chart.js
+++ src/modules/c/d3chart/d3chart.js
@@ -10,38 +10,45 @@ export default class Atsite_assetClassChart extends LightningElement {
   
   @api tableData;
   @api tableDataSize;
-  @api totalAsset;
-  @api assetClassValue;
+  @api totalAsset;//= '10000';
+  @api assetClassValue;// = { Stocks: 1000, Bonds: 2000, Balanced: 3000, Cash: 4000, Other: 5000 };
   @api timeNow;
 
+  assetClassValue;
+
+  // get assetclass() {
+  //   return this.assetClassValue;
+  // }
+
+  // @api
+  // set assetclass(value) {
+  //   this.initializeD3(value);
+  //   this.assetClassValue = value;
+  // }
+
+
   get hasValue() {
+    // this.tableData ? true : false; // surprisingly this doesn't work in the template
     if(this.tableData)
     {
-      this.initializeD3();
       return true;
     }
     return false;
   }
-  
 
 
   renderedCallback() {
-    // if (this.d3Initialized) {
-    //   return;
-    // }
-
-
-
-    
-
-
-
-    // this.d3Initialized = true;
+    setTimeout(() => {
+      if(this.assetClassValue)
+      {
+        this.initializeD3(this.assetClassValue);
+      }
+      
+    }, 1000);
   }
 
-  initializeD3() {
 
-    // remove the d3 chart
+  initializeD3(data) {
     d3.select(this.template.querySelector(".donut_chart")).selectAll("*").remove();
 
     const width = 325,
@@ -71,11 +78,11 @@ export default class Atsite_assetClassChart extends LightningElement {
       };
       */
 
-    const donutChartData = this.assetClassValue;
+    const donutChartData = data;
 
     const color = d3
       .scaleOrdinal()
-      .domain(Object.keys(this.assetClassValue))
+      .domain(Object.keys(data))
       .range(this.color_scheme1);
 
     const pie = d3
@@ -93,7 +100,6 @@ export default class Atsite_assetClassChart extends LightningElement {
       .innerRadius(radius * 0.75) // This is the size of the donut hole
       .outerRadius(radius * 0.78);
 
-    // const innerRadiusColor = ["#FFFFFF", "#0A294A"];
     // Another arc that won't be drawn. Just for labels positioning
     const innerArc = d3
       .arc()
@@ -149,51 +155,51 @@ export default class Atsite_assetClassChart extends LightningElement {
       .text("Total Asset");
   }
 
-  // handleMouseOut(event) {
-  //   this.template
-  //     .querySelector("svg")
-  //     .querySelectorAll(".chart-slice")
-  //     .forEach((slice) => {
-  //       slice.style.opacity = 1;
-  //     });
+  handleMouseOut(event) {
+    this.template
+      .querySelector("svg")
+      .querySelectorAll(".chart-slice")
+      .forEach((slice) => {
+        slice.style.opacity = 1;
+      });
 
-  //   this.template
-  //     .querySelector("svg")
-  //     .querySelector(".asset-value").textContent = this.totalAsset;
+    this.template
+      .querySelector("svg")
+      .querySelector(".asset-value").textContent = this.totalAsset;
 
-  //   this.template
-  //     .querySelector("svg")
-  //     .querySelector(".asset-label").textContent = "Total Asset";
-  // }
+    this.template
+      .querySelector("svg")
+      .querySelector(".asset-label").textContent = "Total Asset";
+  }
 
-  // // event listener in table row
-  // handleMouseOver(event) {
-  //   var parent = event.target.parentElement;
-  //   var assetType = parent.getAttribute("data-asset-type");
-  //   console.log(assetType);
+  // event listener in table row
+  handleMouseOver(event) {
+    var parent = event.target.parentElement;
+    var assetType = parent.getAttribute("data-asset-type");
+    console.log(assetType);
 
-  //   var assetValue = parent.getAttribute("data-asset-value");
-  //   console.log(assetValue);
+    var assetValue = parent.getAttribute("data-asset-value");
+    console.log(assetValue);
 
-  //   var chartSlice = "slice-" + assetType;
-  //   console.log(chartSlice);
+    var chartSlice = "slice-" + assetType;
+    console.log(chartSlice);
 
-  //   this.template
-  //     .querySelector("svg")
-  //     .querySelectorAll(".chart-slice")
-  //     .forEach((slice) => {
-  //       if (slice.classList.contains(chartSlice)) {
-  //         slice.style.opacity = 1;
-  //       } else {
-  //         slice.style.opacity = 0.2;
-  //       }
-  //     });
+    this.template
+      .querySelector("svg")
+      .querySelectorAll(".chart-slice")
+      .forEach((slice) => {
+        if (slice.classList.contains(chartSlice)) {
+          slice.style.opacity = 1;
+        } else {
+          slice.style.opacity = 0.2;
+        }
+      });
 
-  //   this.template
-  //     .querySelector("svg")
-  //     .querySelector(".asset-value").textContent = assetValue;
-  //   this.template
-  //     .querySelector("svg")
-  //     .querySelector(".asset-label").textContent = assetType;
-  // }
+    this.template
+      .querySelector("svg")
+      .querySelector(".asset-value").textContent = assetValue;
+    this.template
+      .querySelector("svg")
+      .querySelector(".asset-label").textContent = assetType;
+  }
 }
